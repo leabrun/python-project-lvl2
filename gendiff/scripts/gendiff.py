26dd6b147bcc17cc8f6c84fa8dc65ca683db36a3
gReen7aRRow <f.gusarov@inbox.ru>
2022-03-10 19:11:41 +0300

    first step

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Generate diff')
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+
+    args = parser.parse_args()
+    print(args.accumulate(args.integers))
+
+
+if __name__ == '__main__':
+    main()
